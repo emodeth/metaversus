6555d310658f3364999889d2c6aca1f40b4890b8
emodeth <emodethistaken@gmail.com>
2023-09-27 18:43:16 +0300

    Explore section completed

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textContainer, textVariant, textVariant2 } from "../utils/motion";
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -16,4 +16,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-2 font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+);
